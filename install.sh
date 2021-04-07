b7416bdbba66d0db0de80b8ac2701298f0ab3a37
Nicolas Arkkila <nicolas@arkkila.fi>
2021-04-07 19:43:04 +0300

    Append instead of write

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ date +%s > ~/.pwnbox_start
 mkdir ~/bin
 export PATH=~/bin:$PATH
 wget https://raw.githubusercontent.com/narkkil/tmux-pwnbox-timer/master/tmux-pwnbox-timer -o ~/bin/tmux-pwnbox-timer
-echo "set -g status-right '#(tmux-pwnbox-timer)'" > ~/.tmux.conf
+echo "set -g status-right '#(tmux-pwnbox-timer)'" >> ~/.tmux.conf
