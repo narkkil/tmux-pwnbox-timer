6fd4d174d1c2bb648b46913964d5f1c437fec63e
Nicolas Arkkila <nicolas@arkkila.fi>
2021-04-07 19:49:07 +0300

    Thanks wget
    -o -> -O

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ date +%s > ~/.pwnbox_start
 
 mkdir -p ~/bin
 export PATH=~/bin:$PATH
-wget https://raw.githubusercontent.com/narkkil/tmux-pwnbox-timer/master/tmux-pwnbox-timer -o ~/bin/tmux-pwnbox-timer
+wget https://raw.githubusercontent.com/narkkil/tmux-pwnbox-timer/master/tmux-pwnbox-timer -O ~/bin/tmux-pwnbox-timer
 chmod u+x ~/bin/tmux-pwnbox-timer
 echo "set -g status-right '#(tmux-pwnbox-timer)'" >> ~/.tmux.conf
