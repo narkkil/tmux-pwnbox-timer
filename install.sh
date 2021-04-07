7386b4216cf40c1397cf91a12df1cedf29910482
Nicolas Arkkila <nicolas@arkkila.fi>
2021-04-07 19:41:18 +0300

    Use HTTPS

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ date +%s > ~/.pwnbox_start
 
 mkdir ~/bin
 export PATH=~/bin:$PATH
-wget http://raw.githubusercontent.com/narkkil/tmux-pwnbox-timer/master/tmux-pwnbox-timer -o ~/bin/tmux-pwnbox-timer
+wget https://raw.githubusercontent.com/narkkil/tmux-pwnbox-timer/master/tmux-pwnbox-timer -o ~/bin/tmux-pwnbox-timer
 echo "set -g status-right '#(tmux-pwnbox-timer)'" > ~/.tmux.conf
