32d11a8cef1a44263149867d9214f183d57a14b2
Nicolas Arkkila <nicolas@arkkila.fi>
2021-04-07 19:45:57 +0300

    chmod plugin to allow execution

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ date +%s > ~/.pwnbox_start
 mkdir ~/bin
 export PATH=~/bin:$PATH
 wget https://raw.githubusercontent.com/narkkil/tmux-pwnbox-timer/master/tmux-pwnbox-timer -o ~/bin/tmux-pwnbox-timer
+chmod u+x ~/bin/tmux-pwnbox-timer
 echo "set -g status-right '#(tmux-pwnbox-timer)'" >> ~/.tmux.conf
